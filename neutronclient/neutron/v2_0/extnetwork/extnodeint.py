50c5e09fefa567bfbd8e1b046cb8a28b483d046a
mfernandes <manuelfernandes55@ua.pt>
2016-05-02 17:09:02 +0100

    ...

--- neutronclient/neutron/v2_0/extnetwork/extnodeint.py
+++ neutronclient/neutron/v2_0/extnetwork/extnodeint.py
@@ -2,7 +2,6 @@ import argparse
 
 from neutronclient._i18n import _
 from neutronclient.common import extension
-from neutronclient.common import utils
 from neutronclient.neutron import v2_0 as neutronV20
 
 
