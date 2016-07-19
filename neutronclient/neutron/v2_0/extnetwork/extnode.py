207b42f005a7d0970fc4c606d4ad8934dd5b790c
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-19 10:30:40 +0100

    ...

--- neutronclient/neutron/v2_0/extnetwork/extnode.py
+++ neutronclient/neutron/v2_0/extnetwork/extnode.py
@@ -24,6 +24,7 @@ def add_know_arguments(parser):
 
     parser.add_argument(
         '--topology-discover', dest='topology_discover',
+        action='store_true',
         help=_('Run the topology discover.'))
 
 
