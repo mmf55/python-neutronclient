c789747dc8d8d506a236ab7cc38802a71834cf6d
Manuel Martins Fernandes <manuelfernandes55@ua.pt>
2016-07-22 11:41:07 +0100

    ...

--- neutronclient/neutron/v2_0/extnetwork/extnode.py
+++ neutronclient/neutron/v2_0/extnetwork/extnode.py
@@ -38,7 +38,7 @@ def args2body(body, parsed_args):
 class ExtNodeCreate(extension.ClientExtensionCreate, ExtNode):
     shell_command = 'extnode-create'
 
-    list_columns = ['id', 'name', 'ip_address', 'info']
+    list_columns = ['id', 'name', 'ip_address', 'topology_discover_info']
 
     def add_known_arguments(self, parser):
         add_know_arguments(parser)
