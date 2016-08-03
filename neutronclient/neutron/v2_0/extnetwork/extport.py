ccfd1d4a4133235f26d2dfdc55df2c99d1d87b98
mfernandes <manuelfernandes55@ua.pt>
2016-08-03 20:23:38 +0100

    ...

--- neutronclient/neutron/v2_0/extnetwork/extport.py
+++ neutronclient/neutron/v2_0/extnetwork/extport.py
@@ -10,10 +10,16 @@ class UpdatePortToExtPortMixin(object):
 
     def add_arguments_extport(self, parser):
         parser.add_argument(
-            '--extinterface-id',
+            '--extinterface-name',
             default=argparse.SUPPRESS,
-            dest='extinterface_id',
-            help=_('Set this port to be an external port by defining the external interface that it will be attached.'))
+            dest='extinterface_name',
+            help=_('Used if is desired one specific external port on the requested node.'))
+
+        parser.add_argument(
+            '--extinterface-name',
+            default=argparse.SUPPRESS,
+            dest='extinterface_name',
+            help=_('External node name of the node which the port will be placed'))
 
     def args2body_extport(self, parsed_args, body):
-        neutronV20.update_dict(parsed_args, body, ['extinterface_id'])
+        neutronV20.update_dict(parsed_args, body, ['extinterface_name', 'extnode_name'])
